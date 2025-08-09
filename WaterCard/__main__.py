1513eef100a922b2dabd1ef1f5fb7c6a6023865c
AlbertUnruh <73029826+AlbertUnruh@users.noreply.github.com>
2025-08-09 13:12:55 +0200

    add colors to `__main__` (v1.1.1)

--- WaterCard/__main__.py
+++ WaterCard/__main__.py
@@ -3,17 +3,27 @@
 from WaterCard import generate
 
 
-kwargs = {
-    "name": input("Your name (press ENTER to be Anonymous): "),
-    "title": input("Your title (press ENTER to be unemployed): "),
-    "mastodon": input("Your primary Mastodon (ENTER to skip): "),
-    "mastodon1": input("Your secondary Mastodon (ENTER to skip): "),
-    "email": input("Your primary email (ENTER to skip): "),
-    "email1": input("Your secondary email (ENTER to skip): "),
-    "phone": input("Your primary phone (ENTER to skip): "),
-    "phone1": input("Your secondary phone (ENTER to skip): "),
-    "website": input("Your primary website (ENTER to skip): "),
-    "website1": input("Your secondary website (ENTER to skip): "),
+PRIMARY: str = "\033[1;33mprimary\033[0m"
+SECONDARY: str = "\033[1;36msecondary\033[0m"
+ENTER_TO_SKIP: str = "\033[3m(press \033[1;2mENTER\033[0m\033[3m to skip)\033[0m"
+
+
+def _(s: str) -> str:
+    """Make text go magenta."""
+    return f"\033[1;35m{s}\033[0m"
+
+
+kwargs: dict[str, str] = {
+    "name": input(f"Your {_("name")} \033[3m(press \033[1;2mENTER\033[0m\033[3m to be Anonymous)\033[0m: "),
+    "title": input(f"Your {_("title")} \033[3m(press \033[1;2mENTER\033[0m\033[3m to be unemployed)\033[0m: "),
+    "mastodon": input(f"Your {PRIMARY} {_("Mastodon")} {ENTER_TO_SKIP}: "),
+    "mastodon1": input(f"Your {SECONDARY} {_("Mastodon")} {ENTER_TO_SKIP}: "),
+    "email": input(f"Your {PRIMARY} {_("email")} {ENTER_TO_SKIP}: "),
+    "email1": input(f"Your {SECONDARY} {_("email")} {ENTER_TO_SKIP}: "),
+    "phone": input(f"Your {PRIMARY} {_("phone")} {ENTER_TO_SKIP}: "),
+    "phone1": input(f"Your {SECONDARY} {_("phone")} {ENTER_TO_SKIP}: "),
+    "website": input(f"Your {PRIMARY} {_("website")} {ENTER_TO_SKIP}: "),
+    "website1": input(f"Your {SECONDARY} {_("website")} {ENTER_TO_SKIP}: "),
 }
 
 pdf = generate(**{k: v for k, v in kwargs.items() if v})
